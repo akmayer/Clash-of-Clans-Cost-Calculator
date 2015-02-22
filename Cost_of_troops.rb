b945d614bf9f7c121a1653bbc5b62d006959ab0f
Ron Mayer <ramayer+git@gmail.com>
2015-02-22 15:51:00 -0800

    add calculation for time

--- Cost_of_troops.rb
+++ Cost_of_troops.rb
@@ -1,18 +1,26 @@
 def dark_elixir_cost
-
-dark_elixir_cost = {
+	dark_elixir_cost = {
 	'valkyrie' => [70,100,130,160],
 	}
-	end
+end
 
 def elixir_cost
-
 	elixir_cost = {
 		'barbarian' => [25,40,60,80,100,150,200],
 		'archer' => [50,80,120,160,200,300,400],
 		'goblin' => [25,40,60,80,100,150],
 		'wizard' => [1500,2000,2500,3000,3500,4000],
 		'balloon' => [2000,2500,3000,3500,4000,4500],
+		}
+end
+
+def time_cost
+	elixir_cost = {
+		'barbarian' => [20]*7,
+		'archer' => [25]*7,
+		'goblin' => [30]*7,
+		'wizard' => [8*60]*7,
+		'balloon' => [8*60]*7,
 		}
 end
 
@@ -27,19 +35,23 @@ def show_costs(army_description)
 	army = army_description.scan(/([0-9]+) L([0-9]+) ([a-z]+)/)
 	total_elixir_cost = 0
 	total_dark_elixir_cost = 0
+	total_time = 0
 	army.each{|number,level,troops|
 		troop = troops.sub(/s$/,"")
+
 		dark_elixir_for_one = get_cost(dark_elixir_cost,troop,level)
 		elixir_for_one = get_cost(elixir_cost,troop,level)	    
+		time_for_one = get_cost(elixir_cost,troop,level)	    
+
 		total_elixir_cost = total_elixir_cost + elixir_for_one * number.to_i
 		total_dark_elixir_cost = total_dark_elixir_cost + dark_elixir_for_one * number.to_i
-	
+		total_time = total_time + time_for_one * number.to_i
+
 		elixir_for_all = elixir_for_one * number.to_i
 		dark_elixir_for_all = dark_elixir_for_one * number.to_i
 	
-	
 	}
-	puts "The cost of\n #{army_description} is\n #{total_elixir_cost} elixir and #{total_dark_elixir_cost} dark elixir."	
+	puts "The cost of\n #{army_description} is\n #{total_elixir_cost} elixir and #{total_dark_elixir_cost} dark elixir and #{total_time} seconds."	
 end
 show_costs("50 L5 barbarians, 25 L4 wizards, and 50 L4 archers")
 show_costs("8 L5 balloons, 80 L4 barbarian, and 80 L4 archers")
